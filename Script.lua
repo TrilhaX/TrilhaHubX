d654d606e1a2030aa2ade93f0b4747e1378f79c4
TrilhaX <141366739+TrilhaX@users.noreply.github.com>
2023-08-04 22:27:39 -0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -9,32 +9,122 @@ getgenv().autoNarutoEgg = true
 
 getgenv().SpinWheel = true
 
+getgenv().ClaimGift = true
+
 function autoAttack()
     while getgenv().autoAttack == true do
         game:GetService("ReplicatedStorage").Remote.ClickerDamage:FireServer()
         wait(0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010)
     end
 end
-
-function autoNarutoEgg()
-    while getgenv().autoNarutoEgg do
-        wait()
-        local args = {
-            [1] = workspace.Worlds.Naruto.NarutoEgg,
-            [2] = 999
-            
-        }
-        game:GetService("ReplicatedStorage").Remote.OpenEgg:InvokeServer(unpack(args))
-        
-        end
-    end    
-
-
+ 
     function SpinWheel()
         while getgenv().SpinWheel == true do
             game:GetService("ReplicatedStorage").Remote.DailySpin:FireServer()     
             end
         end
+
+        function ClaimGift()
+            while getgenv().ClaimGift == true do
+                wait()
+                local args = {
+                    [1] = 1
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+                
+                local args = {
+                    [1] = 2
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+                
+                local args = {
+                    [1] = 3
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+                
+                local args = {
+                    [1] = 4
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+                
+                local args = {
+                    [1] = 5
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+
+                local args = {
+                    [1] = 6
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+                
+                local args = {
+                    [1] = 7
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+
+                local args = {
+                    [1] = 8
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+
+                local args = {
+                    [1] = 9
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+
+                local args = {
+                    [1] = 10
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+
+                local args = {
+                    [1] = 11
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+
+                local args = {
+                    [1] = 12
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+
+                local args = {
+                    [1] = 13
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+
+                local args = {
+                    [1] = 14
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+
+                local args = {
+                    [1] = 15
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+                
+                local args = {
+                    [1] = 16
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ClaimGift:FireServer(unpack(args))
+
+                end
+            end
 
 local FarmTab = Window:MakeTab({
     Name = "Farm",
@@ -52,22 +142,6 @@ Callback = function(Value)
 end    
 })
 
-local EggTab = Window:MakeTab({
-    Name = "Egg",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
-
-
-
-EggTab:AddToggle({
-Name = "Auto Hatch Naruto Egg",
-Default = false,
-Callback = function(Value)
-    getgenv().autoNarutoEgg = Value
-    autoNarutoEgg()
-end    
-})
 
 local MiscTab = Window:MakeTab({
     Name = "Misc",
@@ -85,6 +159,21 @@ Callback = function(Value)
     SpinWheel()
 end    
 })
+
+local args = {
+    [1] = 1
+}
+
+
+MiscTab:AddToggle({
+    Name = "Claim Gift",
+    Default = false,
+    Callback = function(Value)
+        getgenv().ClaimGift = Values
+        ClaimGift()
+    end    
+    })
+
 
 end
 OrionLib:Init()
